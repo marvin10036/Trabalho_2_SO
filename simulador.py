dde92ddfc13994797e01b056a2abb742a01c5711
João Vitor Duarte Domingos <jv.dd.senai.sc@gmail.com>
2023-06-19 19:21:31 -0300

    comentários sobre o que é cada estrutura

--- simulador.py
+++ simulador.py
@@ -3,11 +3,11 @@ from gerando_listas import generate_input_list_random, generate_input_list_with_
 from LRU import LRU
 from FIFO import FIFO
 
-max_page_frames = int(input("Escreva o número de quadros (page frames): "))
-max_pages = int(input("Escreva o número de paginas na tabela de paginas: "))
+max_page_frames = int(input("Escreva o número de quadros (page frames): ")) #10
+max_page_table_size = int(input("Escreva o número de paginas na tabela de paginas: ")) #100
 
-input_list_size = int(input("Escreva o número de referencias a memorias que devem ocorrer: "))
-Input_List = generate_input_list_with_repetition(input_list_size, max_pages)
+input_list_size = int(input("Escreva o número de referencias a memorias que devem ocorrer: ")) #1.000.000
+Input_List = generate_input_list_with_repetition(input_list_size, max_page_table_size) #lista em si
 #Input_List = generate_input_list_random(input_list_size, max_pages)
 
 #MP é a mesma não importando o algoritmo
@@ -19,7 +19,7 @@ print(Input_List)
 
 ###main, page_number, Input_List, MP, max_page_frames
 
-page_faults = LRU(max_pages, Input_List, MP, max_page_frames)
+page_faults = LRU(max_page_table_size, Input_List, MP, max_page_frames)
 print(page_faults)
 
 #MP = []
