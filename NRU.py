e7d075cedd2a264c3efcca4f1bd8892f5f7037be
João Vitor Duarte Domingos <jv.dd.senai.sc@gmail.com>
2023-06-20 23:24:51 -0300

    ajuste NRU

--- NRU.py
+++ NRU.py
@@ -9,17 +9,21 @@ class Page:
 
 def NRU(mp: [], max_page_frames, inputList: []):
     page_faults_counter = 0
+    num_entradas = 0
     for page_number in inputList:
-        if len(mp) < max_page_frames:
-            mp.append(Page(page_number))
+        if num_entradas == max_page_frames:
+            clear_reference(mp)
+        num_entradas += 1
+        if reference_page(mp, page_number):
+            pass
         else:
-            if reference_page(mp, page_number):
-                pass
+            page_faults_counter += 1
+            if len(mp) < max_page_frames:
+                mp.append(Page(page_number))
             else:
-                page_faults_counter += 1
                 mp.remove(select_victim_page(mp))
-
-    return page_faults_counter
+                mp.append(Page(page_number))
+        return page_faults_counter
 
 
 def reference_page(pages: [Page], page_number):
@@ -30,13 +34,17 @@ def reference_page(pages: [Page], page_number):
     return False
 
 
+def clear_reference(pages):
+    for page in pages:
+        page.referenced = False
+
+
 def select_victim_page(pages):
     class_index = {0: [], 1: []}
 
     for page in pages:
         index = int(page.referenced)
         class_index[index].append(page)
-        page.referenced = False
 
     for i in range(2):
         if len(class_index[i]) > 0:
